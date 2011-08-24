2314083baebba9423cf0316a7c05bfc48a3d5ad5
Jesse Lankila <kheldysh@kapsi.fi>
2011-08-25 02:24:29 +0300

    loadbalancer-before

--- server_lb.rb
+++ server_lb.rb
@@ -7,14 +7,20 @@ require 'net/http'
 
 enable :sessions
 
+before do
+  if not session[:server_for_this_session]
+    servers = ['http://localhost:10000', 'http://localhost:10001']
+    random_index = rand(servers.length)
+    session[:server_for_this_session] = servers[random_index] # forward randomly
+  end
+end
+
 get '/session' do
   session[:testing] = 'hello'
 end
 
 get '*' do
-  servers = ['http://localhost:10000', 'http://localhost:10001']
-  random_index = rand(servers.length)
-  server = servers[random_index] # forward randomly
+  server = session[:server_for_this_session]
   path = params[:splat][0] # first member of array should be the path
   uri = URI.parse(server + path)
   response = Net::HTTP.get_response(uri)
