270bba37efec2589d5af58e5f96ecf1b493b3a0b
Jesse Lankila <kheldysh@kapsi.fi>
2011-08-25 01:12:44 +0300

    httpbasics-asquerystring

--- hello.rb
+++ hello.rb
@@ -6,20 +6,24 @@ before do
   @redis = Redis.new
 end
 
-post '/hello/:who' do
+post '/hello' do
   @redis.set 'hello', params[:who]
-  "ok" # implicit return statement
+  'ok' # implicit return statement
+end
+
+get '/hello' do
+  @redis.get 'hello' # lessee if implicit return works here
 end
 
 get '/redis' do
-  @redis.set "hello", "world"
+  @redis.set 'hello', 'world'
   raise 'is this thing on?'
-  value = @redis.get "hello"
+  value = @redis.get 'hello'
   return value
 end
 
 get '/' do
-  "Hello, world!"
+  'Hello, world!'
 end
 
 
