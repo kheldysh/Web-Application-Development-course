dfd651867cdfa51fb884a056cc24e7181ce4eaec
Jesse Lankila <kheldysh@kapsi.fi>
2011-08-25 00:47:16 +0300

    Now creates an instance of Redis before handling requests

--- hello.rb
+++ hello.rb
@@ -2,11 +2,19 @@ require 'sinatra'
 require 'redis'
 require 'sinatra/reloader'
 
+before do
+  @redis = Redis.new
+end
+
+post '/hello/:who' do
+  @redis.set 'hello', params[:who]
+  "ok" # implicit return statement
+end
+
 get '/redis' do
-  redis = Redis.new
-  redis.set "hello", "world"
+  @redis.set "hello", "world"
   raise 'is this thing on?'
-  value = redis.get "hello"
+  value = @redis.get "hello"
   return value
 end
 
