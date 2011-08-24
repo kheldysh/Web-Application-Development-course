edeac5aafb29474f5c1f21957c8346d4b765629e
Jesse Lankila <kheldysh@kapsi.fi>
2011-08-25 02:13:11 +0300

    loadbalancer-addb

--- server_lb.rb
+++ server_lb.rb
@@ -6,9 +6,11 @@ require 'sinatra/reloader'
 require 'net/http'
 
 get '*' do
-  server_a = 'http://localhost:10000'
+  servers = ['http://localhost:10000', 'http://localhost:10001']
+  random_index = rand(servers.length)
+  server = servers[random_index] # forward randomly
   path = params[:splat][0] # first member of array should be the path
-  uri = URI.parse(server_a + path)
+  uri = URI.parse(server + path)
   response = Net::HTTP.get_response(uri)
   return response.body
 end
