a7b1df0693fb120a6107d239c69c0767e6343a5b
Jesse Lankila <kheldysh@kapsi.fi>
2011-08-25 04:20:23 +0300

    chat-getnpost

---
+++ chat.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'redis'
+require 'sinatra/reloader'
+
+before do
+  @redis = Redis.new
+end
+
+get '/messages' do
+  messages = @redis.lrange "messages", -3, -1
+  return messages.inspect
+end
+
+post '/messages' do
+  @redis.rpush "messages", params[:message]
+end
