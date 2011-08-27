dae0f00b472e6b6f15837c8a63f8bc6fb5f81a38
Jesse Lankila <kheldysh@kapsi.fi>
2011-08-27 21:50:51 +0300

    soa-chat

--- composition.rb
+++ composition.rb
@@ -7,7 +7,14 @@ Server_A = 'http://localhost:10000'
 Server_B = 'http://localhost:10001'
 Server_Chat = 'http://localhost:10003'
 
-get '*' do
+post '/messages' do # forward /messages requests to chat 
+  uri = URI.parse(Server_Chat + '/messages')
+  res = Net::HTTP.post_form(uri, {'message' => params[:message]})
+  redirect '/'
+end
+    
+
+get '/' do
   path = '/whoami'
   uri = URI.parse(Server_A + path)
   @from_server_a = get_response_body_from(uri)
