03e57a48965a239e1133fae16fd91fba9080e6b2
Jesse Lankila <kheldysh@kapsi.fi>
2011-08-28 16:50:59 +0300

    restformats-usingjson

--- ragetool/app/controllers/exercises_controller.rb
+++ ragetool/app/controllers/exercises_controller.rb
@@ -1,10 +1,13 @@
 class ExercisesController < ApplicationController
 
+  skip_before_filter :authenticate_user!
+
   def index
     @exercises = Exercise.all
     
     respond_to do |format|
       format.xml { render :xml => @exercises.to_xml }
+      format.json { render :json => @exercises.to_json }
       format.html
     end
     
@@ -15,6 +18,7 @@ class ExercisesController < ApplicationController
 
     respond_to do |format|
       format.xml { render :xml => @exercise.to_xml }
+      format.json { render :json => @exercises.to_json }
       format.html
     end
   end
